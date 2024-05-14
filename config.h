aef4b934b8b8de2c3b7d0af21d11bda7429a25f4
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2024-05-14 14:52:29 +0300

    config.h | Adapted the blocks for my status scripts.

--- config.h
+++ config.h
@@ -1,34 +1,38 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"⌨", "sb-kbselect", 0, 30}, */
+	{"",	"sb-brightness",	        0,	                5},
+	{"",	"sb-volume",	                0,	                4},
+	{"",	"sb-mode",	                0,	                3},
+	{"",	"sb-date",	                60,	                2},
+	{"",	"sb-time",	                60,	                1},
+	/*
+	{"⌨", "sb-kbselect", 0, 30},
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	{"",	"sb-tasks",	10,	26},
-	/* {"",	"sb-music",	0,	11}, */
+	{"",	"sb-music",	0,	11},
 	{"",	"sb-pacpackages",	0,	8},
 	{"",	"sb-news",		0,	6},
-	/* {"",	"sb-price xmr-btc \"Monero to Bitcoin\" 🔒 25",	9000,	25}, */
-	/* {"",	"sb-price xmr Monero 🔒 24",			9000,	24}, */
-	/* {"",	"sb-price eth Ethereum 🍸 23",			9000,	23}, */
-	/* {"",	"sb-price btc Bitcoin 💰 21",			9000,	21}, */
+	{"",	"sb-price xmr-btc \"Monero to Bitcoin\" 🔒 25",	9000,	25},
+	{"",	"sb-price xmr Monero 🔒 24",			9000,	24},
+	{"",	"sb-price eth Ethereum 🍸 23",			9000,	23},
+	{"",	"sb-price btc Bitcoin 💰 21",			9000,	21},
 	{"",	"sb-torrent",	20,	7},
-	/* {"",	"sb-memory",	10,	14}, */
-	/* {"",	"sb-cpu",		10,	18}, */
-	/* {"",	"sb-moonphase",	18000,	17}, */
+	{"",	"sb-memory",	10,	14},
+	{"",	"sb-cpu",		10,	18},
+	{"",	"sb-moonphase",	18000,	17},
 	{"",	"sb-doppler",	0,	13},
 	{"",	"sb-forecast",	18000,	5},
 	{"",	"sb-mailbox",	180,	12},
 	{"",	"sb-nettraf",	1,	16},
-	{"",	"sb-volume",	0,	10},
-	{"",	"sb-battery",	5,	3},
-	{"",	"sb-clock",	60,	1},
 	{"",	"sb-internet",	5,	4},
-	/* {"",	"sb-iplocate", 0,	27}, */
+	{"",	"sb-iplocate", 0,	27},
 	{"",	"sb-help-icon",	0,	15},
+	*/
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = " | ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
